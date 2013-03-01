02e181d576bded21d7a47caa81f5cc00749b8907
Ali Faraji <phpbbpersian@gmail.com>
2013-03-01 21:54:00 +0200

    1.0.0

--- root/profile_comments_mod_install.php
+++ root/profile_comments_mod_install.php
@@ -28,7 +28,7 @@ if (!file_exists($phpbb_root_path . 'umil/umil_auto.' . $phpEx))
 }
 
 // The name of the mod to be displayed during installation.
-$mod_name = 'Comment on Posts';
+$mod_name = 'ACP_PROFILE_COMMENTS';
 
 /*
 * The name of the config variable which will hold the currently installed version
